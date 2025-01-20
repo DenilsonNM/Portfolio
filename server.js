47298fa70b316db6e613da050db503263c5740b5
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2025-01-20 17:50:22 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = req.url === '/' ? 'index.html' : req.url;
+  const ext = path.extname(filePath);
+  const contentType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+  }[ext] || 'text/plain';
+
+  fs.readFile(path.join(__dirname, filePath), (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
