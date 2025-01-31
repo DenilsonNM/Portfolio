92b1f6043898b90535278155c359b998b6a027cf
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2025-01-31 16:56:33 -0600

    tabs v2, focus and active colors

--- docs/js/tabs.js
+++ docs/js/tabs.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
   $(".tab_content").hide();
-  $(".ul_tabs li:first").addClass("active").show();
+  $(".ul_tabs li:first").addClass("active");
   $(".tab_content:first").show();
 
-  $("ul.ul_tabs li").click(function () {
+  $("ul.ul_tabs li").click(function (event) {
+    event.preventDefault();
+
     $(".ul_tabs li").removeClass("active");
-    $(this).addClass("active");
+    $(this).addClass("active"); // clase activa al <li> clicado
+
     $(".tab_content").hide();
 
-    var activeTab = $(this).find("a").attr("href");
+    var activeTab = $(this).find("a").attr("href"); // ID de la pestaña
     $(activeTab).fadeIn();
-    return false;
   });
 });
