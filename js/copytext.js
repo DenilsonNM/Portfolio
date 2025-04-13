6d764c6ab9c1eb50c26d8fc9072b021130f7635c
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2025-04-12 21:25:20 -0600

    section center version 2 in progress

--- js/copytext.js
+++ js/copytext.js
@@ -3,9 +3,9 @@ var copytext = document.getElementById("copytext");
 
 copybtn.onclick = function () {
   navigator.clipboard.writeText(copytext.innerHTML);
-
-  copybtn.innerHTML = "Copied"
-  setTimeout(function () {
-    copybtn.innerHTML = "Copy"
-  }, 2000)
+  alert("denilsonjnm31@gmail.com");
+  // copybtn.innerHTML = "Copied"
+  // setTimeout(function () {
+  //   copybtn.innerHTML = "Copy"
+  // }, 2000)
 }
