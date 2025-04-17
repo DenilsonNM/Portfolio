2ea84d1d46ae6597ba3cf3661aabb70c2ea1e49f
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2025-04-16 20:43:28 -0600

    typingeffect.js

--- js/typingeffect.js
+++ js/typingeffect.js
@@ -1,4 +1,4 @@
-const phrases = ["Systems Engineer", "Jr. Frontend Developer"];
+const phrases = ["Computer Systems Engineer", "Jr. Frontend Developer"];
 const textElement = document.getElementById("typingeffect");
 let phraseIndex = 0;
 let charIndex = 0;
